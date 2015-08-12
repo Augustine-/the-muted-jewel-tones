c04de6020e6f1acb121a821495beb26ea47c1e1c
Augustine Wittkower <augustine.wittkower@gmail.com>
2015-08-12 14:20:07 -0700

    init

---
+++ stripes.js
@@ -0,0 +1,19 @@
+(function(){
+  setInterval(function(){
+    var colors = [
+    '#D2100B', 
+    '#F5892d', 
+    '#F5AE2D', 
+    '#FFDF55', 
+    '#86C40A',
+    '#078F58',
+    '#077A7F',
+    '#4353B7',
+    '#6F47B3',
+    '#880788'
+    ];
+  }, 6000);
+});
+
+// we're going to assign each color to a stripe, one at a time 
+// every six seconds or so
