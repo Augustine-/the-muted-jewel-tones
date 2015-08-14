70f2e7498c366dd1b50679f76f12c6a1856e9b12
Augustine Wittkower <augustine.wittkower@gmail.com>
2015-08-13 21:04:00 -0700

    stripey scrolley multiple feaux-window extravaganza

--- stripes.js
+++ stripes.js
@@ -24,5 +24,31 @@ $(document).ready((function(){
   function shuffle(o){
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
-}
+  }
+
+
 }));
+
+
+$.fn.moveIt = function(){
+  var $els = $(this);
+  var $window = $(window);
+  var scrollPos = $window.scrollTop();
+  
+  $window.on('scroll', function(){
+    scrollPos = $window.scrollTop();
+    $els.each(moveEl);
+  });
+  
+  function moveEl(){
+    var $this = $(this);
+    var scrollSpeed = $this.data('scroll-speed');
+    var elPos = scrollPos / scrollSpeed;
+
+    $this.css('transform', 'translateY(-' + elPos + 'px)');
+  }
+}
+
+$(function(){
+  $('[data-scroll-speed]').moveIt();
+});
