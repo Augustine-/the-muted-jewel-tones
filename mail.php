ba1e8280ef616fc2edcf754150a4f907f0743a9e
Augustine Wittkower <augustine.wittkower@gmail.com>
2015-08-14 20:47:15 -0700

    added error reporting for php mailer

--- mail.php
+++ mail.php
@@ -3,11 +3,11 @@
 $name        = $_POST['name'];
 $email       = $_POST['email'];
 $message     = $_POST['message'];
-$formcontent ="From: $name \n Message: $message";
+$formcontent = "From: $name \n Message: $message";
 $recipient   = "augustine.wittkower@gmail.com";
 $subject     = "Contact Form";
 $mailheader  = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail($recipient, $subject, $formcontent, $mailheader) or die(print_r(error_get_last()));
 echo "Thank You!";
 
- ?>
+?>
